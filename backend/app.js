6d598fe4aa60938ebc8e43ea0d375de5a0e095ff
Orlando <orlando01otero@gmail.com>
2023-09-13 11:11:54 -0400

    backend setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
