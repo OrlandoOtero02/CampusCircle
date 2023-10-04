c5c8510980d91ade056faefd218638e505d71f29
OrlandoOtero02 <ouotero@purdue.edu>
2023-10-04 12:21:51 -0400

    able to delete from circles from user-schema

--- frontend/src/components/CircleDetails.js
+++ frontend/src/components/CircleDetails.js
@@ -1,9 +1,22 @@
 const CircleDetails = ({ circle }) => {
+
+    const handleClick = async() => {
+        const response = await fetch('/home/circles/' + circle._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            console.log(json)
+        }
+    }
+
     return(
         <div className="circle-details">
             <h4>{circle.title}</h4>
             <p>Description: {circle.description}</p>
             <p>{circle.createdAt}</p>
+            <span onClick={handleClick}>delete</span>
         </div>
     )
 }
