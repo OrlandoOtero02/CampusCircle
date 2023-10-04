fa33dd07e11167a7a7c5919eca9199492041a1e0
OrlandoOtero02 <ouotero@purdue.edu>
2023-10-04 15:12:14 -0400

    email and password signup backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,33 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+//static signup method
+userSchema.statics.signup = async function (email, password) {
+
+    //validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password is not strong enough')
+    }
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
