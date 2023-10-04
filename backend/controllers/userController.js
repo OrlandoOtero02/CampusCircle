63600caca63d6270d97d4e6cbe3ab48f585699b3
OrlandoOtero02 <ouotero@purdue.edu>
2023-10-04 15:32:17 -0400

    json web Token for auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,27 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // login user
 const loginUser = async (req, res) => {
+    
     res.json({msg: 'login user'})
 }
 
 // signup user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body
+    const {email, password, username} = req.body
 
     try {
-        const user = await User.signup(email, password)
+        const user = await User.signup(email, password, username)
 
-        res.status(200).json({email, user})
+        //create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,14 +13,19 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
     }
 })
 
 //static signup method
-userSchema.statics.signup = async function (email, password) {
+userSchema.statics.signup = async function (email, password, username) {
 
     //validation
-    if (!email || !password) {
+    if (!email || !password || !username) {
         throw Error('All fields must be filled')
     }
     if (!validator.isEmail(email)) {
@@ -36,10 +41,16 @@ userSchema.statics.signup = async function (email, password) {
         throw Error('Email already in use')
     }
 
+    const existsU = await this.findOne({ username })
+
+    if (existsU) {
+        throw Error('Username already in use')
+    }
+
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
 
-    const user = await this.create({ email, password: hash })
+    const user = await this.create({ email, password: hash, username})
 
     return user
 }
