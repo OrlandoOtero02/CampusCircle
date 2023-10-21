39278269745141712d2863e0b15b63ed72881977
Jooheon Lee <lee3036@purdue.edu>
2023-10-21 16:32:13 -0400

    added delete account button and popup on settings page

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function Settings() {
+  const [open, setOpen] = useState(false);
+
+  const handleDeleteAccount = () => {
+    // Call the API delete function here
+    // API call or any related logic goes here
+    // For now, just logging a message
+    console.log('Deleting account...');
+    setOpen(false); // Close the dialog
+  };
+
   return (
     <div className="Settings">
       <div className="settings-container">
@@ -18,6 +34,25 @@ function Settings() {
             <h3>Notifications</h3>
             {/* Add your notifications settings options here */}
           </div>
+          <div className="settings-section">
+            <Button variant="contained" color="secondary" onClick={() => setOpen(true)}>
+              Delete Account
+            </Button>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+              <DialogTitle>Confirm Account Deletion</DialogTitle>
+              <DialogContent>
+                <DialogContentText>Are you sure you want to delete your account?</DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleDeleteAccount} color="secondary">
+                  Yes
+                </Button>
+                <Button onClick={() => setOpen(false)} color="primary">
+                  Cancel
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
         </div>
       </div>
     </div>
@@ -25,4 +60,3 @@ function Settings() {
 }
 
 export default Settings;
-
