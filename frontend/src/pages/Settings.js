fe40a4b0b459b765c5944fad5afc5f698b585438
amritavivek <vivekana@purdue.edu>
2023-11-03 02:54:27 -0400

    Added reset password functionality from within settings page

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Settings() {
+  const [showChangePassword, setShowChangePassword] = useState(false);
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  const { user: currentUser } = useAuthContext();
+
+  const handleResetPassword = (e) => {
+    e.preventDefault();
+    // Validation logic here
+    if (newPassword === confirmNewPassword) {
+      // Call API to update password or handle the password change logic
+      console.log('Password has been changed to:', newPassword);
+      // Reset form or give user feedback
+      setNewPassword('');
+      setConfirmNewPassword('');
+      setShowChangePassword(false); // Hide the form after submitting
+
+      axios.patch('/api/user/updatePassword', { email: currentUser.email, newPassword: newPassword })
+          .then((response) => {
+              // Handle the response, e.g., show a success message
+              console.log(response.data.message);
+              // Redirect user or give feedback
+          })
+          .catch((error) => {
+              // Handle any errors
+              console.error(error.response.data.error);
+          });
+  
+      console.log('New password is set:', newPassword);
+    } else {
+      // Handle the error scenario here
+      console.log('Passwords do not match');
+    }
+  };
+
   return (
     <div className="Settings">
       <div className="settings-container">
@@ -8,15 +44,38 @@ function Settings() {
           <h2>Settings Page</h2>
           <div className="settings-section">
             <h3>General Settings</h3>
-            {/* Add your general settings options here */}
+            {/* General settings options */}
           </div>
           <div className="settings-section">
             <h3>Security Settings</h3>
-            {/* Add your security settings options here */}
+            {/* Security settings options */}
+            <button onClick={() => setShowChangePassword(!showChangePassword)} className="change-password-btn">
+              Change Password
+            </button>
+            {showChangePassword && (
+              <form onSubmit={handleResetPassword}>
+                <h2>Set New Password</h2>
+                <input
+                  type="password"
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  value={newPassword}
+                  placeholder="Enter new password"
+                  required
+                />
+                <input
+                  type="password"
+                  onChange={(e) => setConfirmNewPassword(e.target.value)}
+                  value={confirmNewPassword}
+                  placeholder="Confirm new password"
+                  required
+                />
+                <button type="submit">Set New Password</button>
+              </form>
+            )}
           </div>
           <div className="settings-section">
             <h3>Notifications</h3>
-            {/* Add your notifications settings options here */}
+            {/* Notifications settings options */}
           </div>
         </div>
       </div>
@@ -25,4 +84,3 @@ function Settings() {
 }
 
 export default Settings;
-
