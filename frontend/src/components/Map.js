52ccec044179583277b473d32aab43b1d962cfea
OrlandoOtero02 <ouotero@purdue.edu>
2023-12-05 13:53:54 -0500

    map in good shape

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -6,21 +6,42 @@ import mapboxgl from 'mapbox-gl';
 const Map = () => {
   useEffect(() => {
     // Set your Mapbox access token here
-    mapboxgl.accessToken = 'pk.eyJ1Ijoib3VvdGVybyIsImEiOiJjbHByaTM1c2owNzd1MnZuem9hMmQyZnRtIn0.PaxC4oEBghK-pUjAeZnPrA';
+    mapboxgl.accessToken = 'pk.eyJ1Ijoib3VvdGVybyIsImEiOiJjbHByazhzc2IwMGt4MmtvNmRzbWJqOXVvIn0.RJRdRiC_gkuahLAsrkbDFA';
 
     // Create a new map instance
     const map = new mapboxgl.Map({
       container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      style: 'mapbox://styles/mapbox/standard', // 3D style URL
+      center: [-86.913780, 40.428640], // starting position [lng, lat]
+      zoom: 18, // starting zoom,
+      pitch: 45, // starting pitch in degrees
+      maxBounds: [[-86.930515, 40.420158], [-86.910465, 40.435776]]
+    });
+
+    // Add 3D building layer
+    map.on('load', () => {
+      // Add a 3D building layer
+      map.addLayer({
+        id: '3d-buildings',
+        source: 'composite',
+        'source-layer': 'building',
+        filter: ['==', 'extrude', 'true'],
+        type: 'fill-extrusion',
+        minzoom: 15,
+        paint: {
+          'fill-extrusion-color': '#aaa',
+          'fill-extrusion-height': ['get', 'height'],
+          'fill-extrusion-base': ['get', 'min_height'],
+          'fill-extrusion-opacity': 0.8
+        }
+      });
     });
 
     // Cleanup the map when the component unmounts
     return () => map.remove();
   }, []); // The empty dependency array ensures that this effect runs once
 
-  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+  return <div id="map" style={{ width: '100%', height: '650px' }} />;
 };
 
 export default Map;
--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -3,7 +3,6 @@ import Map from "../components/Map"
 const MapPage = () => {
     return (
         <div>
-          <h1>Your Social Meetup App</h1>
           <Map />
         </div>
       );
