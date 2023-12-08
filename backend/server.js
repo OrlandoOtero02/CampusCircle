5497020cd602f9847dbb895c479de8cb5ae3e0b9
Rudis Sprenne <70488838+rsprenne@users.noreply.github.com>
2023-12-07 20:14:36 -0500

    Began implementing messaging, has errors

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const express = require('express')
 
 const mongoose = require('mongoose')
 
+const http = require('http');
+
+const socketIo = require('socket.io');
+
 // routes
 const circlesRoutes = require('./routes/circles')
 const userRoutes = require('./routes/user')
@@ -12,6 +16,10 @@ const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
 
 // middleware
 app.use(express.json())
@@ -25,6 +33,7 @@ app.use((req, res, next) => {
 app.use('/api/circles', circlesRoutes)
 app.use('/api/user', userRoutes)
 
+const port = 5000;
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI, {dbName: 'CampusCircle'})
@@ -37,4 +46,28 @@ mongoose.connect(process.env.MONGO_URI, {dbName: 'CampusCircle'})
     .catch((error) => {
         console.log(error)
     })
-
+    
+    io.on('connection', (socket) => {
+        console.log('User connected');
+      
+        socket.on('message', (messageData) => {
+          const { text, user } = messageData;
+      
+          const message = new Message({ text, user });
+          message.save((err) => {
+            if (err) {
+              console.error('Error saving message to MongoDB:', err);
+            } else {
+              io.emit('message', messageData);
+            }
+          });
+        });
+      
+        socket.on('disconnect', () => {
+          console.log('User disconnected');
+        });
+      });
+      
+      server.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
--- frontend/src/pages/Messaging.js
+++ frontend/src/pages/Messaging.js
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from 'react';
-//import io from 'socket.io-client';
-//import axios from 'axios';
+import io from 'socket.io-client';
+import axios from 'axios';
 import { useAuthContext } from '../hooks/useAuthContext';
-import { 
-    ChatContainer,
-    MessageList,
-    Message,
-    MessageInput,
-} from '@chatscope/chat-ui-kit-react';
 
-//const socket = io('http://your-server-url'); // Replace with your server URL
+const socket = io('http://localhost:3000'); // Replace with your server URL
 
 function Messaging() {
   const [message, setMessage] = useState('');
@@ -19,38 +13,48 @@ function Messaging() {
 
   const handleSendMessage = () => {
     if (message) {
-//     socket.emit('message', { text: message, user });
-//      setMessage('');
+     socket.emit('message', { text: message, user });
+      setMessage('');
     }
   };
 
   useEffect(() => {
-//    socket.on('message', (messageData) => {
-//      setMessages((prevMessages) => [...prevMessages, messageData]);
-//    });
+    socket.on('message', (messageData) => {
+      setMessages((prevMessages) => [...prevMessages, messageData]);
+    });
 
     // Fetch previous messages when the component mounts
-//    axios.get('http://your-server-url/messages').then((response) => {
-//      setMessages(response.data);
-//    });
+    axios.get('http://localhost:3000/messaging').then((response) => {
+      setMessages(response.data);
+    });
   }, []);
 
   return (
-    <ChatContainer>
-        <MessageList>
-        <Message model={{
-                 message: "Hello my friend",
-                 sentTime: "just now",
-                 sender: "Joe",
-                 direction: "incoming",
-                 type: "text",
-
-                 }} >
-                    <Message.Header sender="Joe"/>
-                    </Message>
-        </MessageList>
-        <MessageInput attachButton='false' placeholder="Type message here" />
-    </ChatContainer>
+    <div>
+      <div className="chat-box">
+        {messages.map((msg, index) => (
+          <div key={index} className="message">
+            <strong>{msg.user}: </strong>
+            {msg.text}
+          </div>
+        ))}
+      </div>
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Your Username"
+          value={user}
+          onChange={(e) => setSender(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
   );
 }
 
